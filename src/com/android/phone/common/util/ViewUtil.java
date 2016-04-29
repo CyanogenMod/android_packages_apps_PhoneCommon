99f1c853b5dede7110383c5bf3f4708205372927
Kamaljeet Maini <kmaini@cyngn.com>
2016-04-28 18:03:04 -0700

    Reduce dialer name text field size to handle rounding error
    The name field of InCallUI calculates maximum possible text size
    for the availalble width. In some cases, this leads to slightly
    bigger text than width of text view. Setting the text size using
    float doesn't always match with expected size. This causes name
    field to display ... at the end and truncate the string.
    
    To prevent this issue, the text size is now reduced by a fixed value
    of 1, which should handle any rounding errors.
    
    Change-Id: I390ca38b1cad29db42d6a1ed52346c6a72dec4a8
    Issue-Id: CYNGNOS-2674

--- src/com/android/phone/common/util/ViewUtil.java
+++ src/com/android/phone/common/util/ViewUtil.java
@@ -95,12 +95,17 @@ public class ViewUtil {
     public static void resizeText(TextView textView, int originalTextSize, int minTextSize) {
         final Paint paint = textView.getPaint();
         final int width = textView.getWidth();
+
+        // Setting text size using float may result in slightly bigger font due to rounding.
+        // Therefore, reducing the text size value by 1 to ensure text always fits in the text view.
+        final float textSizeRoundingError = 1.0f;
+
         if (width == 0) return;
         textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, originalTextSize);
         float ratio = width / paint.measureText(textView.getText().toString());
         if (ratio <= 1.0f) {
             textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    Math.max(minTextSize, originalTextSize * ratio));
+                    Math.max(minTextSize, originalTextSize*ratio - textSizeRoundingError));
         }
     }
 }
