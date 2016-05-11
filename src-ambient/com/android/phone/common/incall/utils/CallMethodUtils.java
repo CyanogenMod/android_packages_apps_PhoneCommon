bdd80a256ef3fc9bf40a50ff61a1f610ac3dd62b
Stephen Bird <sbird@cyngn.com>
2016-05-11 16:39:36 -0700

    [2/2] Preferences for provider roaming call counts
    Ticket: CD-636
    Change-Id: I43c38cb8e98443556e90c8db576d84383a4d79c6

--- src-ambient/com/android/phone/common/incall/utils/CallMethodUtils.java
+++ src-ambient/com/android/phone/common/incall/utils/CallMethodUtils.java
@@ -55,6 +55,7 @@ public class CallMethodUtils {
     public final static String PREF_SPINNER_COACHMARK_SHOW = "pref_spinner_coachmark_shown";
     public final static String PREF_LAST_ENABLED_PROVIDER = "pref_last_enabled_provider";
     public final static String PREF_INTERNATIONAL_CALLS = "pref_international_calls";
+    public final static String PREF_ROAMING_CALLS = "pref_roaming_calls";
     public final static String PREF_WIFI_CALL = "pref_wifi_call";
 
     public static CallMethodInfo getDefaultSimInfo(Context context) {
